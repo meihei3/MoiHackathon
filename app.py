ef5e70a437c9d6c315b6e9dcaa26f71f8fb056a4
yameholo <ame.aroid.xz5@gmail.com>
2018-08-20 18:20:24 +0900

    test comment page

--- app.py
+++ app.py
@@ -14,12 +14,32 @@ app.config['SECRET_KEY'] = config.secret_key
 client_id = config.client_id
 client_secret = config.client_secret
 
-OAUTH2_URL = "https://apiv2.twitcasting.tv/oauth2"
-CATEGORY_URL = "https://apiv2.twitcasting.tv/categories"
+BASE_URL = "https://apiv2.twitcasting.tv"
+OAUTH2_URL = BASE_URL + "/oauth2"
+CATEGORY_URL = BASE_URL + "/categories"
+COMMENT_URL = BASE_URL + "/movies/{movie_id}/comments"
+USER_URL = BASE_URL + "/users/{user_id}"
 
 
 def login_url(ci):
     return OAUTH2_URL + "/authorize?client_id={YOUR_CLIENT_ID}&response_type=code".format(**{"YOUR_CLIENT_ID": ci})
+
+
+def macro_header(access_token):
+    return {"X-Api-Version": "2.0", "Authorization": "Bearer %s" % access_token}
+
+
+def url2user(url, at):
+    """
+    URLからuser objectを返す
+
+    :param url: living url
+    :param at: access_token
+    :return:
+    """
+    user_id = url.split("/")[-1]
+    req = requests.get(USER_URL.format(**{"user_id": user_id}), headers=macro_header(at))
+    return json.loads(req.text)
 
 
 @app.route('/callback')
@@ -40,6 +60,28 @@ def callback():
     data = json.loads(req.text)
     session["access_token"] = data["access_token"]
     return redirect(url_for('index'))
+
+
+@app.route('/comment')
+def comment():
+    movie_id = request.args.get("movie_id")
+    access_token = session.get('access_token')
+    headers = macro_header(access_token)
+    payload = {"limit": 15}
+    req = requests.get(COMMENT_URL.format(**{"movie_id": movie_id}), data=payload, headers=headers)
+    print("comment", req.text)
+    data = json.loads(req.text)
+    return render_template('comment.html', data=data)
+
+
+@app.route('/user')
+def user():
+    user_id = request.args.get('user_id')
+    access_token = session.get('access_token')
+    headers = macro_header(access_token)
+    req = requests.get(USER_URL.format(**{"user_id": user_id}), headers=headers)
+    print("user:", req.text)
+    return req.text
 
 
 @app.route('/')
